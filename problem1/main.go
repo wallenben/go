d184ec7f3fad3c9196f6666d84a40015937a2e3e
wallenben <42152099+wallenben@users.noreply.github.com>
2020-03-24 20:19:40 -0500

    bruh

---
+++ problem1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"fmt"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "bruh")
+	flag.Parse()
+	file.err := os.Open()
+	if err != nil{
+		
+	}else println("you'r file (%s) doesn't work", csvFilename)
+	os.Exit(1)
+}
